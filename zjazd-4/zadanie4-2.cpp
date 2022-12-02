5d731cc63516cd746dee0a3734c25406d581916b
s27814-pj <116372449+s27814-pj@users.noreply.github.com>
2022-12-02 21:12:19 +0100

    Add files via upload
    DESCRIption - wiecej niz 1

--- zjazd-4/zadanie4-2.cpp
+++ zjazd-4/zadanie4-2.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+
+void ZeroThis(int results[], int max) {
+    for (int i = 0; i < max; i++)results[i] = 0;
+}
+
+int CheckForValueInArray(int arra[], int value, int sizeARRAY) {
+    for (int k = 0; k < (sizeARRAY + 1); k++) {
+        if (arra[k] == value) return 1;
+    }
+    return 0;
+}
+
 int main()
 {
     int size = 0;
@@ -12,10 +24,16 @@ int main()
         std::cin >> array[i];
     }
     int HighestCountRepeatedValue = 0;
+    int next = 0;
     int HighestValue = array[0];
+    int* results = new int[size];
+    ZeroThis(results, size);
+    //results = { 0 };
+    //for (int i = 0; i < size; i++) std::cout << results[i];
     for (int j = 0; j < size; j++)
     {
         int countRepeatedValue = 0;
+
         for (int i = 0; i < size; i++)
         {
             if (array[j] == array[i]) countRepeatedValue++;
@@ -23,10 +41,22 @@ int main()
             {
                 HighestCountRepeatedValue = countRepeatedValue;
                 HighestValue = array[j];
+                ZeroThis(results, size);
+                results[0] = HighestValue;
+                next = 0;
+            }
+            else if (countRepeatedValue == HighestCountRepeatedValue) {
+                if (CheckForValueInArray(results, array[j], next) != 1) {
+                    next++;
+                    results[next] = array[i];
+                }
             }
         }
     }
 
-    if (size != 0) std::cout << "liczba wystapien naczestszego elementu = " << HighestCountRepeatedValue << "  / wartosc najczesciej wystepujacego elementu = " << HighestValue << std::endl;
+    if (size != 0) if (next == 0) std::cout << "liczba wystapien naczestszego elementu = " << HighestCountRepeatedValue << "  / wartosc najczesciej wystepujacego elementu = " << HighestValue << std::endl;
+    if (size != 0) if (next > 0) std::cout << "jest kilka roznych elementow ktore sa najczesciej podane:";
+    if (size != 0) if (next > 0) for (int i = 0; i < (next + 1); i++) std::cout << results[i] << " ";
     delete[] array;
+    delete[] results;
 }
