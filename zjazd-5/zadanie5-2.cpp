40da8da0c779490c68c08d76718399cdac3d6aa1
s27814-pj <116372449+s27814-pj@users.noreply.github.com>
2023-01-06 11:28:15 +0100

    Add files via upload

--- zjazd-5/zadanie5-2.cpp
+++ zjazd-5/zadanie5-2.cpp
@@ -5,6 +5,12 @@
 using namespace std;
 
 
+void swapp(int anotherarray[], int a, int b) {
+    int i = anotherarray[b];
+    anotherarray[b] = anotherarray[a];
+    anotherarray[a] = i;
+}
+
 int main() {
 
 
@@ -13,23 +19,47 @@ int main() {
     cout << "Podaj 2 wymiary tablicy(oddzielone spacja), nastepnie po spacji wartosci(liczby calkowite) wszystkich elementow oddzielone spacja" << std::endl;
     cin >> size;
     cin >> moreSize;
-    size = size * moreSize;
+    //size = size * moreSize;
     if (size < 0) size = 0;
     //array<int, size>;
-    int* somearray = new int[size];
+    int* somearray = new int[(size*moreSize)];
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < (size*moreSize); i++)
     {
         cin >> somearray[i];
     }
-
-    for (int i = 0; i < size; i++)
+    //swapp(somearray,1, 2);
+    for (int i = 0; i < (size*moreSize); i++)
     {
         //for (int k = 0; k < moreSize; k++) {
-            if (i % moreSize == 0) cout << endl;
-            else cout << " ";
-            cout << *(somearray + i);
+        if (i % moreSize == 0) cout << endl;
+        else cout << "\t";
+        cout << *(somearray + i);
         //}
-        
+
     }
+    int n = 0;
+    for (int i = 0; i < size; i++) {
+        for (int k = 0; k < moreSize; k++) {
+            if (k == (moreSize / 2)) break;
+            n = k % moreSize;
+            swapp(somearray, (n + (i * moreSize)), ((moreSize - (n + 1)) + (i * moreSize)));
+            //if (k % moreSize == 0) swapp(somearray, (k+(i*size)), ((moreSize - 1)+(i*size)));
+            //if (k == (moreSize / 2)) break;
+        }
+    }
+   
+    cout << endl;
+
+    for (int i = 0; i < (size*moreSize); i++)
+    {
+        //for (int k = 0; k < moreSize; k++) {
+        if (i % moreSize == 0) cout << endl;
+        else cout << "\t";
+        cout << *(somearray + i);
+        //}
+
+    }
+
+    cout << endl;
 }
